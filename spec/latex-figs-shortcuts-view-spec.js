fbfa582110da27a707dba47d2fb492f203c275ae
9sako6 <31821663+9sako6@users.noreply.github.com>
2018-08-13 19:07:28 +0900

    initial commit

---
+++ spec/latex-figs-shortcuts-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LatexFigsShortcutsView from '../lib/latex-figs-shortcuts-view';
+
+describe('LatexFigsShortcutsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
